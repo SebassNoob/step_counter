4bca0303fdbeba5093b2854aefafeaf0aa98ef95
sebass-but-ab <78428559+sebass-but-ab@users.noreply.github.com>
2021-02-26 13:50:12 +0800

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 steps = 0
 cal = 0
-weight = 0
+weight = 50
 state = 0
 setup = 0
-
+currentspd = ""
+spd = ["slow","medium","fast"]
 OLED.init(128, 64)
 
 def on_gesture_shake():
@@ -11,48 +12,92 @@ def on_gesture_shake():
     steps += 1
 input.on_gesture(Gesture.Shake, on_gesture_shake)
 def calc():
-    #todo later
+    global cal
     pass
 
 
 
+
 def keypressed():
-    global state
+    global state,weight,setup
     if tinkercademy.ad_keyboard(ADKeys.A, AnalogPin.P0):
-        OLED.clear()
-        state = 1
-        OLED.write_string("steps = " + str(steps))
-        pause(5000)
-        OLED.clear()
-        state = 0
-        menudisplay()
+        if setup == 0:
+            weight += 1
+            OLED.clear()
+            menudisplay()
+
+        if setup == 1:
+            pass
+
+        if setup == 2:
+            OLED.clear()
+            state = 1
+            OLED.write_string("steps = " + str(steps))
+            pause(5000)
+            OLED.clear()
+            state = 0
+            menudisplay()
 
     if tinkercademy.ad_keyboard(ADKeys.B, AnalogPin.P0):
-        OLED.clear()
-        state =1 
-        OLED.write_string_new_line("calories = " + str(cal))
-        pause (5000)
-        OLED.clear()
-        state = 0
-        menudisplay()
+        if setup == 0:
+            weight -= 1
+            OLED.clear()
+            menudisplay()
+
+        if setup == 2:
+            OLED.clear()
+            state = 1
+            OLED.write_string("calories = " + str(cal))
+            pause(5000)
+            OLED.clear()
+            state = 0
+            menudisplay()
+    
+    if tinkercademy.ad_keyboard(ADKeys.C, AnalogPin.P0):
+        
+        if setup == 0:
+            setup = 1
+            OLED.clear()
+            menudisplay()
+        if setup == 1:
+            setup = 2
+            OLED.clear()
+            menudisplay()
+
 
 
 
 def menudisplay():
-    global state
-    if state == 0:
-        OLED.write_string_new_line("A. Show steps")
-        OLED.write_string_new_line("B. Show calories")
-        OLED.write_string_new_line("C. Input body weight")
+    global state,setup,weight,currentspd
+    if setup == 0:
+        OLED.write_string_new_line("Set weight:" + str(weight) + "kg")
+        OLED.write_string_new_line("Button C to confirm")
+
+    if setup == 1:
+        OLED.write_string_new_line("Set speed of walk:" + str(currentspd) + "kg")
+        OLED.write_string_new_line("Button C to confirm")
+
+    if setup == 2:
+        if state == 0:
+            OLED.write_string_new_line("A. Show steps")
+            OLED.write_string_new_line("B. Show calories")
+            OLED.write_string_new_line("C. Input body weight")
+        elif state == 1:
+            pass
         
         
-        
+def weightensure():
+    global weight
+    if weight <= 0:
+        weight = 0
+    if weight >= 300:
+        weight = 300      
 
     
 
 menudisplay()
 def on_forever():
-    
+    weightensure()
     keypressed()
     calc()
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,54 +1,121 @@
 let steps = 0
 let cal = 0
-let weight = 0
+let weight = 50
 let state = 0
 let setup = 0
+let currentspd = ""
+let spd = ["slow", "medium", "fast"]
 OLED.init(128, 64)
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
     
     steps += 1
 })
 function calc() {
-    // todo later
+    
     
 }
 
 function keypressed() {
     
     if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P0)) {
-        OLED.clear()
-        state = 1
-        OLED.writeString("steps = " + ("" + steps))
-        pause(5000)
-        OLED.clear()
-        state = 0
-        menudisplay()
+        if (setup == 0) {
+            weight += 1
+            OLED.clear()
+            menudisplay()
+        }
+        
+        if (setup == 1) {
+            
+        }
+        
+        if (setup == 2) {
+            OLED.clear()
+            state = 1
+            OLED.writeString("steps = " + ("" + steps))
+            pause(5000)
+            OLED.clear()
+            state = 0
+            menudisplay()
+        }
+        
     }
     
     if (tinkercademy.ADKeyboard(ADKeys.B, AnalogPin.P0)) {
-        OLED.clear()
-        state = 1
-        OLED.writeStringNewLine("calories = " + ("" + cal))
-        pause(5000)
-        OLED.clear()
-        state = 0
-        menudisplay()
+        if (setup == 0) {
+            weight -= 1
+            OLED.clear()
+            menudisplay()
+        }
+        
+        if (setup == 2) {
+            OLED.clear()
+            state = 1
+            OLED.writeString("calories = " + ("" + cal))
+            pause(5000)
+            OLED.clear()
+            state = 0
+            menudisplay()
+        }
+        
+    }
+    
+    if (tinkercademy.ADKeyboard(ADKeys.C, AnalogPin.P0)) {
+        if (setup == 0) {
+            setup = 1
+            OLED.clear()
+            menudisplay()
+        }
+        
+        if (setup == 1) {
+            setup = 2
+            OLED.clear()
+            menudisplay()
+        }
+        
     }
     
 }
 
 function menudisplay() {
     
-    if (state == 0) {
-        OLED.writeStringNewLine("A. Show steps")
-        OLED.writeStringNewLine("B. Show calories")
-        OLED.writeStringNewLine("C. Input body weight")
+    if (setup == 0) {
+        OLED.writeStringNewLine("Set weight:" + ("" + weight) + "kg")
+        OLED.writeStringNewLine("Button C to confirm")
+    }
+    
+    if (setup == 1) {
+        OLED.writeStringNewLine("Set speed of walk:" + ("" + currentspd) + "kg")
+        OLED.writeStringNewLine("Button C to confirm")
+    }
+    
+    if (setup == 2) {
+        if (state == 0) {
+            OLED.writeStringNewLine("A. Show steps")
+            OLED.writeStringNewLine("B. Show calories")
+            OLED.writeStringNewLine("C. Input body weight")
+        } else if (state == 1) {
+            
+        }
+        
+    }
+    
+}
+
+function weightensure() {
+    
+    if (weight <= 0) {
+        weight = 0
+    }
+    
+    if (weight >= 300) {
+        weight = 300
     }
     
 }
 
 menudisplay()
 forever(function on_forever() {
+    weightensure()
     keypressed()
     calc()
 })
