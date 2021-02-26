6ebe0d29489365e39d8185536672a8a8c985cb8f
sebass-but-ab <78428559+sebass-but-ab@users.noreply.github.com>
2021-02-26 13:18:12 +0800

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -2,41 +2,59 @@ steps = 0
 cal = 0
 weight = 0
 state = 0
+setup = 0
+
 OLED.init(128, 64)
+
 def on_gesture_shake():
     global steps
     steps += 1
 input.on_gesture(Gesture.Shake, on_gesture_shake)
 def calc():
-    #todo later 
+    #todo later
     pass
+
+
+
 def keypressed():
+    global state
     if tinkercademy.ad_keyboard(ADKeys.A, AnalogPin.P0):
-        global state
+        OLED.clear()
         state = 1
         OLED.write_string("steps = " + str(steps))
-        pause(1000)
+        pause(5000)
         OLED.clear()
         state = 0
+        menudisplay()
 
     if tinkercademy.ad_keyboard(ADKeys.B, AnalogPin.P0):
-        global state
+        OLED.clear()
         state =1 
-        OLED.write_string_new_line("calories =" + str(cal))
-        pause (1000)
+        OLED.write_string_new_line("calories = " + str(cal))
+        pause (5000)
         OLED.clear()
         state = 0
+        menudisplay()
+
+
+
 def menudisplay():
+    global state
     if state == 0:
-        OLED.write_string("A. Show steps")
+        OLED.write_string_new_line("A. Show steps")
         OLED.write_string_new_line("B. Show calories")
         OLED.write_string_new_line("C. Input body weight")
-    else:
-        OLED.clear()
+        
+        
+        
+
+    
+
+menudisplay()
 def on_forever():
-    menudisplay()
+    
     keypressed()
     calc()
 forever(on_forever)
 
-   
+
--- main.ts
+++ main.ts
@@ -2,50 +2,53 @@ let steps = 0
 let cal = 0
 let weight = 0
 let state = 0
+let setup = 0
 OLED.init(128, 64)
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
     
     steps += 1
 })
 function calc() {
-    // todo later 
+    // todo later
     
 }
 
 function keypressed() {
+    
     if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P0)) {
-        
+        OLED.clear()
         state = 1
         OLED.writeString("steps = " + ("" + steps))
-        pause(1000)
+        pause(5000)
         OLED.clear()
         state = 0
+        menudisplay()
     }
     
     if (tinkercademy.ADKeyboard(ADKeys.B, AnalogPin.P0)) {
-        
+        OLED.clear()
         state = 1
-        OLED.writeStringNewLine("calories =" + ("" + cal))
-        pause(1000)
+        OLED.writeStringNewLine("calories = " + ("" + cal))
+        pause(5000)
         OLED.clear()
         state = 0
+        menudisplay()
     }
     
 }
 
 function menudisplay() {
+    
     if (state == 0) {
-        OLED.writeString("A. Show steps")
+        OLED.writeStringNewLine("A. Show steps")
         OLED.writeStringNewLine("B. Show calories")
         OLED.writeStringNewLine("C. Input body weight")
-    } else {
-        OLED.clear()
     }
     
 }
 
+menudisplay()
 forever(function on_forever() {
-    menudisplay()
     keypressed()
     calc()
 })
